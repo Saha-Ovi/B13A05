34d1479bcc5bfbc7803f6ce69ac163e29dbddfd1
Saha-Ovi <ovisaha3210@gmail.com>
2026-03-07 00:01:58 +0600

    login page created and functional

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputText=document.getElementById("input-text");
+const inputPassword=document.getElementById("input-password");
+document.getElementById("login-btn").addEventListener("click",()=>
+{
+    const userName=inputText.value.toLowerCase();
+    const userPassword=inputPassword.value;
+    if(userName=="admin" && userPassword=="admin123")
+    {
+        alert("Sign In successful");
+        window.location.assign("./index.html");
+    }
+    else
+    {
+        alert("Please Sign In With Proper Name or Password");
+    }
+;})
